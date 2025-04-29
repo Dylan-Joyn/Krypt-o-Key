1c4eb3e93ec2b0502fa317ba460b8370b70e9f29
benps <benpsnook@gmail.com>
2025-04-29 16:33:04 -0500

    FEAT: added UtilFunc class, moved editDistance() to it from TimedTyping

--- src/src/TimedTyping.java
+++ src/src/TimedTyping.java
@@ -125,34 +125,8 @@ public class TimedTyping {
         return timerThread;
     }
 
-
-    public static int editDistance(String s1, String s2) {
-        int lenS1 = s1.length();
-        int lenS2 = s2.length();
-        int[][] dp = new int[lenS1 + 1][lenS2 + 1];
-
-        for (int i = 0; i <= lenS1; i++) {
-            dp[i][0] = i;
-        }
-        for (int j = 0; j <= lenS2; j++) {
-            dp[0][j] = j;
-        }
-
-        for (int i = 1; i <= lenS1; i++) {
-            for (int j = 1; j <= lenS2; j++) {
-                if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
-                    dp[i][j] = dp[i - 1][j - 1];
-                } else {
-                    dp[i][j] = Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1])) + 1;
-                }
-            }
-        }
-        return dp[lenS1][lenS2];
-    }
-
-
     private TypingResult calculateResult(String target, String input) {
-        int editDistance = editDistance(input, target);
+        int editDistance = UtilFunc.editDistance(input, target);
         int maxPossibleDistance = Math.max(input.length(), target.length());
 
         double accuracy = (1 - (double) editDistance / maxPossibleDistance) * 100;
